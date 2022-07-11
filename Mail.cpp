aade45c264438ab498cb1dbfaf714258b73d944d
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-11 15:18:41 +0300

    Update Mail.cpp

--- Mail.cpp
+++ Mail.cpp
@@ -1,9 +1,19 @@
 #include "Mail.h"
 int Mail::Mail_Value = 1;
 
-Mail::Mail(string Date, string Send_From, string Send_To, string Subject, string Content) : m_Date(Date), m_Send_From(Send_From), m_Send_To(Send_To), m_Subject(Subject), m_Content(Content)//ctor
+Mail::Mail(string Date, string** Send_From, string** Send_To, string Subject, string Content) : m_Date(Date),m_Subject(Subject), m_Content(Content)//ctor
 {
 	Mail_special_num = Mail_Value;
+	int size = *(&Send_From + 1) - Send_From;
+	for (int i = 0; i < size; i++)
+	{
+		m_Send_From=Send_From[i];
+	}
+	int size1 = *(&Send_To + 1) - Send_To;
+	for (int i = 0; i < size1; i++)
+	{
+		m_Send_To=Send_To[i];
+	}
 	Mail_Value++;
 }
 Mail::~Mail()
@@ -19,14 +29,6 @@ string Mail::get_Date() const
 {
 	return m_Date;
 }
-string Mail::get_Send_From() const
-{
-	return m_Send_From;
-}
-string Mail::get_Send_To() const
-{
-	return m_Send_To;
-}
 string Mail::get_Subject() const
 {
 	return m_Subject;
@@ -39,4 +41,24 @@ ostream& operator<<(ostream& os, const Mail& c)
 {
 	os << c.get_Content() <<endl;
 	return os;
+}
+string* Recived_mail::get_Send_From()
+{
+	return m_Send_From;
+}
+string* Recived_mail::get_Send_To()
+{
+	return m_Send_To;
+}
+string* Sent_mail::get_Send_From()
+{
+	return m_Send_From;
+}
+string* Sent_mail::get_Send_To()
+{
+	return m_Send_To;
+}
+int Mail::get_uniqe_id()const
+{
+	return Mail_special_num;
 }
