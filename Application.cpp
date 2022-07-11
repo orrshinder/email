dbf22c0a701f5e4769d4a8c50645af0e15a99031
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-11 09:38:51 +0300

    Create Application.cpp

---
+++ Application.cpp
@@ -0,0 +1,65 @@
+#include "Application.h"
+Application::Application()
+{
+
+}
+Application::~Application()
+{
+
+}
+void Application::Compose()
+{
+
+}
+void Application::Add(string name)
+{
+
+}
+void Application::Remove(string name)
+{
+
+}
+void Application::List()
+{
+
+}
+void Application::AddToList(string rlist_name)
+{
+
+}
+void Application::RemoveFromList(string rlist_name)
+{
+
+}
+void Application::Back()
+{
+
+}
+void Application::Search(string search_text)
+{
+
+}
+void Application::Move(int id_mail, string folder)
+{
+
+}
+void Application::More(string folder_name)
+{
+
+}
+void Application::CD(string folder_name)
+{
+
+}
+void Application::MkDir(string new_folder)
+{
+
+}
+void Application::RmDir(string folder_name)
+{
+
+}
+void Application::ListDir()
+{
+
+}
