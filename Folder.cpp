ebc8c058fa89a1b722e89ad048110c8d86b2e797
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-11 15:16:52 +0300

    Update Folder.cpp

--- Folder.cpp
+++ Folder.cpp
@@ -6,3 +6,23 @@ Folder::Folder(string name)
 {
 	m_name = name;
 }
+void Folder::Move(Mail* mail)
+{
+	mail_list.emplace(mail_list.begin(), &mail);
+}
+
+
+Mail* Folder::Remove(Mail* mail)
+{
+	int counter = 0;
+	for (auto it = mail_list.begin(); it != mail_list.end(); ++it)
+	{
+		if (mail->get_uniqe_id() == it->get_uniqe_id())
+		{
+			Mail* x = &mail_list.at(counter);
+			mail_list.erase(it);
+			return x;
+		}
+		counter++;
+	}
+}
