7090fd7aaa775afe5c8c8779b6e4aa0f1c616acc
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-11 15:19:07 +0300

    Update Mail.h

--- Mail.h
+++ Mail.h
@@ -5,27 +5,34 @@
 #include <iostream>
 #include <string>
 #include "Folder.h"
+#include "Application.h"
+#include "Contacts.h"
 using namespace std;
 
 
 class Mail
 {
 public:
-	Mail(string Date, string Send_From, string Send_To, string Subject, string Content);
+	friend class Folder;
+	friend class Contacts;
+	friend class Contact;
+	friend class Application;
+	Mail(string Date, string** Send_From, string** Send_To, string Subject, string Content);
 	~Mail();
 	string get_Date() const;
-	string get_Send_From() const;
-	string get_Send_To() const;
+	virtual string* get_Send_From() const=0;
+	virtual string* get_Send_To() const=0;
 	string get_Subject() const;
 	string get_Content() const;
+	int get_uniqe_id()const;
 
 	friend ostream& operator<<(ostream& os, const Mail& c);
 
 protected:
 
 	string m_Date;
-	string  m_Send_From;
-	string  m_Send_To;
+	string*  m_Send_From;
+	string*  m_Send_To;
 	string  m_Subject;
 	string  m_Content;
 	int Mail_special_num;
@@ -37,14 +44,18 @@ protected:
 
 class Recived_mail : public Mail
 {
-	Recived_mail(string Date, string Send_From, string Send_To, string Subject, string Content):Mail(Date,Send_From,Send_To,Subject,Content) {};
+	Recived_mail(string Date, string** Send_From, string** Send_To, string Subject, string Content):Mail(Date,Send_From,Send_To,Subject,Content) {};
 	~Recived_mail() {};
+	string* get_Send_From();
+	string* get_Send_To();
 };
 
 class Sent_mail: public Mail
 {
-	Sent_mail(string Date, string Send_From, string Send_To, string Subject, string Content) :Mail(Date, Send_From, Send_To, Subject, Content) {};
+	Sent_mail(string Date, string** Send_From, string** Send_To, string Subject, string Content) :Mail(Date, Send_From, Send_To, Subject, Content) {};
 	~Sent_mail() {};
+	string* get_Send_From();
+	string* get_Send_To();
 };
 
 #endif
