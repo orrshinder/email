09818d1ddbee562984356a449604de183dc0c7d1
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-11 09:41:15 +0300

    Update Mail.h

--- Mail.h
+++ Mail.h
@@ -1,24 +1,25 @@
+#pragma once
 #ifndef MAIL_H
 #define MAIL_H
 
 #include <iostream>
 #include <string>
-
+#include "Folder.h"
 using namespace std;
 
 
 class Mail
 {
 public:
-	Mail(string Date, string Send_From, string Send_To, string Subject, string Content, int Mail_Value);
+	Mail(string Date, string Send_From, string Send_To, string Subject, string Content);
 	~Mail();
 	string get_Date() const;
 	string get_Send_From() const;
 	string get_Send_To() const;
 	string get_Subject() const;
 	string get_Content() const;
-	int get_Mail_Value() const;
 
+	friend ostream& operator<<(ostream& os, const Mail& c);
 
 protected:
 
@@ -27,7 +28,23 @@ protected:
 	string  m_Send_To;
 	string  m_Subject;
 	string  m_Content;
-	int  Mail_Value;
+	int Mail_special_num;
+	static int Mail_Value;
 
 
 };
+
+
+class Recived_mail : public Mail
+{
+	Recived_mail(string Date, string Send_From, string Send_To, string Subject, string Content):Mail(Date,Send_From,Send_To,Subject,Content) {};
+	~Recived_mail() {};
+};
+
+class Sent_mail: public Mail
+{
+	Sent_mail(string Date, string Send_From, string Send_To, string Subject, string Content) :Mail(Date, Send_From, Send_To, Subject, Content) {};
+	~Sent_mail() {};
+};
+
+#endif
