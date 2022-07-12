b26b3c418714491e6eeeec4bc80895d23a536062
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-12 17:53:04 +0300

    Update Mail.cpp

--- Mail.cpp
+++ Mail.cpp
@@ -1,18 +1,19 @@
 #include "Mail.h"
 int Mail::Mail_Value = 1;
 
-Mail::Mail(string Date, string** Send_From, string** Send_To, string Subject, string Content) : m_Date(Date),m_Subject(Subject), m_Content(Content)//ctor
+Mail::Mail(string Date, string** Send_From, string** Send_To, string Subject, string Content, int mail_id) : m_Date(Date),m_Subject(Subject), m_Content(Content),Mail_special_num(mail_id)//ctor
 {
-	Mail_special_num = Mail_Value;
 	int size = *(&Send_From + 1) - Send_From;
+	m_Send_From = new string * [size];
 	for (int i = 0; i < size; i++)
 	{
-		m_Send_From=Send_From[i];
+		m_Send_From[i] = Send_From[i];
 	}
 	int size1 = *(&Send_To + 1) - Send_To;
+	m_Send_To = new string * [size1];
 	for (int i = 0; i < size1; i++)
 	{
-		m_Send_To=Send_To[i];
+		m_Send_To[i] = Send_To[i];
 	}
 	Mail_Value++;
 }
@@ -42,23 +43,32 @@ ostream& operator<<(ostream& os, const Mail& c)
 	os << c.get_Content() <<endl;
 	return os;
 }
-string* Recived_mail::get_Send_From()
+string* Recived_mail::get_Send_From() const
 {
-	return m_Send_From;
+	return *m_Send_From;
 }
-string* Recived_mail::get_Send_To()
+string* Recived_mail::get_Send_To() const
 {
-	return m_Send_To;
+	return *m_Send_To;
 }
-string* Sent_mail::get_Send_From()
+string* Sent_mail::get_Send_From() const
 {
-	return m_Send_From;
+	return *m_Send_From;
 }
-string* Sent_mail::get_Send_To()
+string* Sent_mail::get_Send_To() const
 {
-	return m_Send_To;
+	return *m_Send_To;
 }
-int Mail::get_uniqe_id()const
+int Mail::get_uniqe_id() const
 {
 	return Mail_special_num;
+}
+void  Mail::seralization(ofstream& ofs)
+{
+	ofs << "Date:" << this->get_Date() << endl;
+	ofs <<"sent from:" << this->get_Send_From() << endl;
+	ofs << "sent to:" << this->get_Send_To() << endl;
+	ofs <<"subject:" << this->get_Subject() << endl;
+	ofs << "content:" << this->get_Content() << endl;
+	ofs <<"uniq id:" << this->get_uniqe_id() << endl;
 }
