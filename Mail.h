34302c358b33c58602ccd8e1df96aeaf1bca1500
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-12 17:53:41 +0300

    Update Mail.h

--- Mail.h
+++ Mail.h
@@ -7,8 +7,11 @@
 #include "Folder.h"
 #include "Application.h"
 #include "Contacts.h"
+#include "Contact.h"
 using namespace std;
-
+#define _CRTDBG_MAP_ALLOC
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
 
 class Mail
 {
@@ -17,22 +20,22 @@ public:
 	friend class Contacts;
 	friend class Contact;
 	friend class Application;
-	Mail(string Date, string** Send_From, string** Send_To, string Subject, string Content);
-	~Mail();
+	Mail(string date, string** send_From, string** send_To, string subject, string content, int mail_id);
+	virtual ~Mail();
 	string get_Date() const;
 	virtual string* get_Send_From() const=0;
 	virtual string* get_Send_To() const=0;
 	string get_Subject() const;
 	string get_Content() const;
 	int get_uniqe_id()const;
-
+	void seralization(ofstream& ofs);
 	friend ostream& operator<<(ostream& os, const Mail& c);
 
 protected:
 
 	string m_Date;
-	string*  m_Send_From;
-	string*  m_Send_To;
+	string**  m_Send_From;
+	string**  m_Send_To;
 	string  m_Subject;
 	string  m_Content;
 	int Mail_special_num;
@@ -44,18 +47,20 @@ protected:
 
 class Recived_mail : public Mail
 {
-	Recived_mail(string Date, string** Send_From, string** Send_To, string Subject, string Content):Mail(Date,Send_From,Send_To,Subject,Content) {};
+public:
+	Recived_mail(string Date, string** Send_From, string** Send_To, string Subject, string Content, int mail_id):Mail(Date,Send_From,Send_To,Subject,Content,mail_id) {};
 	~Recived_mail() {};
-	string* get_Send_From();
-	string* get_Send_To();
+	string* get_Send_From() const;
+	string* get_Send_To() const;
 };
 
 class Sent_mail: public Mail
 {
-	Sent_mail(string Date, string** Send_From, string** Send_To, string Subject, string Content) :Mail(Date, Send_From, Send_To, Subject, Content) {};
+public:
+	Sent_mail(string Date, string** Send_From, string** Send_To, string Subject, string Content, int mail_id) :Mail(Date, Send_From, Send_To, Subject, Content,mail_id) {};
 	~Sent_mail() {};
-	string* get_Send_From();
-	string* get_Send_To();
+	string* get_Send_From() const;
+	string* get_Send_To() const;
 };
 
 #endif
