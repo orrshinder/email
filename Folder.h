c3034ef2109c8aba879a06197ee35e070db9276f
orrshinder <101317902+orrshinder@users.noreply.github.com>
2022-07-15 12:07:38 +0300

    Update Folder.h

--- Folder.h
+++ Folder.h
@@ -20,14 +20,21 @@ public:
 	friend class Application;
 	Folder(string m_name);
 	~Folder();
-	void Move(Mail* mail);
-	Mail* Remove(Mail* mail);
+	void Remove(Mail* mail);
 	void Add_old_mail(Mail* mail);
 	void Add_new_mail(Mail* mail);
 	void seralization(ofstream& ofs);
+	bool operator ==(const Folder* other);
+	void push(Mail* c);
+	void push_back(Mail* c);
+	bool isFull() const;
+	bool isEmpty();
+	unsigned size()const;
 
 protected:
 	string m_name;
-	vector<Mail> mail_list;
+	Mail** mail_list;
+	int m_size; 
+	int m_top;
 };
 #endif
